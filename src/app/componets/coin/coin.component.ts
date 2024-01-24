dab2db913edeff5f38830b6882da6b1406a9cc7b
Rami Zayed <ramiwzayed@gmail.com>
2024-01-24 18:51:38 +0200

    Added coin component

---
+++ src/app/componets/coin/coin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coin',
+  standalone: true,
+  imports: [],
+  templateUrl: './coin.component.html',
+  styleUrl: './coin.component.scss'
+})
+export class CoinComponent {
+
+}
