69c307ea9900d34d5a93e6c0c462b3f89cbe7efa
Extreme <ramiwzayed@gmail.com>
2024-03-13 22:01:14 +0200

    Updated board.class.ts

--- src/app/classes/board.class.ts
+++ src/app/classes/board.class.ts
@@ -1,152 +1,110 @@
 export class Board {
-    board: {
-        isFilled: boolean,
-        playerId: number | null,
-    }[][] = []
+  board: {
+    isFilled: boolean,
+    playerId: number | null,
+  }[][] = []
 
-    numberOfRows: number = 7;
-    numberOfColumns: number = 6;
+  numberOfRows: number = 6;
+  numberOfColumns: number = 7;
 
-    constructor() {
-        this.reset()
-        console.log(this.board)
+  constructor() {
+    this.reset()
+    console.log(this.board)
+  }
+
+  reset(): void {
+    for (let i = 0; i < this.numberOfRows; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < this.numberOfColumns; j++) {
+        this.board[i][j] = {
+          isFilled: false,
+          playerId: null
+        }
+      }
+    }
+  }
+
+  drop(row: number, column: number, playerId: number): void {
+    this.board[row][column] = {
+      isFilled: true,
+      playerId: playerId
+    }
+  }
+
+  winnerCheck(playerId: number): boolean {
+    let counter = 0;
+
+    // Rows
+
+    for (let i = 0; i < this.numberOfRows; i++) {
+      for (let j = 0; j < this.numberOfColumns; j++) {
+        const slot = this.board[i][j]
+        if (slot.isFilled && slot.playerId === playerId) {
+          counter++
+        } else if (counter === 4) {
+          return true;
+        } else {
+          counter = 0;
+        }
+
+      }
     }
 
-    reset(): void {
-        // Rows
-        // for (let i = 0; i < this.numberOfRows; i++) {
-        //     this.grid[i] = [];
-        //     for (let j = 0; j < this.numberOfColumns; j++) {
-        //         this.grid[i][j] = {
-        //             isFilled: i === 1 && j < 4,
-        //             playerId: i === 1 && j < 4 ? 1 : null
-        //         }
-        //     }
-        // }
+    counter = 0
 
-        // Columns
-        // for (let i = 0; i < this.numberOfRows; i++) {
-        //     this.grid[i] = [];
-        //     for (let j = 0; j < this.numberOfColumns; j++) {
-        //         this.grid[i][j] = {
-        //             isFilled: j === 6,
-        //             playerId: j === 6 ? 1 : null
-        //         }
-        //     }
-        // }
-
-        // Diagonal
-        // for (let i = 0; i < this.numberOfRows; i++) {
-        //     this.grid[i] = [];
-        //     for (let j = 0; j < this.numberOfColumns; j++) {
-        //         this.grid[i][j] = {
-        //             isFilled: i === j,
-        //             playerId: i === j ? 1 : null
-        //         }
-        //     }
-        // }
-        // for (let i = 0; i < this.numberOfRows; i++) {
-        //     this.grid[i] = [];
-        //     for (let j = 0; j < this.numberOfColumns; j++) {
-        //         this.grid[i][j] = {
-        //             isFilled: i + j === this.numberOfColumns - 1,
-        //             playerId: i + j === this.numberOfColumns - 1 ? 1 : null
-        //         }
-        //     }
-        // }
-
-        for (let i = 0; i < this.numberOfRows; i++) {
-            this.board[i] = [];
-            for (let j = 0; j < this.numberOfColumns; j++) {
-                this.board[i][j] = {
-                    isFilled: false,
-                    playerId: null
-                }
-            }
+    // Columns
+    for (let i = 0; i < this.numberOfColumns; i++) {
+      for (let j = 0; j < this.numberOfRows; j++) {
+        const slot = this.board[j][i];
+        if (slot.isFilled && slot.playerId === playerId) {
+          counter++
+        } else if (counter === 4) {
+          return true;
+        } else {
+          counter = 0;
         }
+
+      }
     }
 
-    drop(row: number, column: number, playerId: number): void {
-        this.board[row][column] = {
-            isFilled: true,
-            playerId: playerId
+    counter = 0
+
+    // Left Diagonal
+    for (let i = 0; i < this.numberOfRows; i++) {
+      for (let j = 0; j < this.numberOfColumns; j++) {
+        const slot = this.board[i][j];
+        if (i === j) {
+          if (slot.isFilled && slot.playerId === playerId) {
+            counter++;
+          } else if (counter === 4) {
+            return true;
+          } else {
+            counter = 0;
+          }
+
         }
+      }
     }
 
-    winnerCheck(playerId: number): boolean {
-        let counter = 0;
+    counter = 0
 
-        // Rows
-        for (let i = 0; i < this.numberOfRows; i++) {
-            const row = this.board[i];
+    // Right Diagonal
+    for (let i = 0; i < this.numberOfRows; i++) {
+      for (let j = 0; j < this.numberOfColumns; j++) {
+        const slot = this.board[i][j];
+        if (i + j === this.numberOfColumns - 1) {
+          if (slot.isFilled && slot.playerId === playerId) {
+            counter++;
+          } else if (counter === 4) {
+            return true;
+          } else {
+            counter = 0;
+          }
 
-            for (let j = 0; j < row.length; j++) {
-                const slot = row[0];
-                if (slot.isFilled && slot.playerId === playerId) {
-                    counter++
-                } else {
-                    counter = 0;
-                }
-                if (counter === 4) {
-                    return true;
-                }
-            }
         }
-
-        counter = 0
-
-        // Columns
-        for (let i = 0; i < this.numberOfColumns; i++) {
-            for (let j = 0; j < this.numberOfRows; j++) {
-                const slot = this.board[j][i];
-                if (slot.isFilled && slot.playerId === playerId) {
-                    counter++
-                } else {
-                    counter = 0;
-                }
-                if (counter === 4) {
-                    return true;
-                }
-            }
-        }
-
-        counter = 0
-
-        // Diagonal
-        for (let i = 0; i < this.numberOfRows; i++) {
-            for (let j = 0; j < this.numberOfColumns; j++) {
-                const slot = this.board[i][j];
-                if (i === j) {
-                    if (slot.isFilled && slot.playerId === playerId) {
-                        counter++;
-                    } else {
-                        counter = 0;
-                    }
-                    if (counter === 4) {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        counter = 0
-
-        for (let i = 0; i < this.numberOfRows; i++) {
-            for (let j = 0; j < this.numberOfColumns; j++) {
-                const slot = this.board[i][j];
-                if (i + j === this.numberOfColumns - 1) {
-                    if (slot.isFilled && slot.playerId === playerId) {
-                        counter++;
-                    } else {
-                        counter = 0;
-                    }
-                    if (counter === 4) {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
+      }
     }
+
+    return false;
+  }
 }
